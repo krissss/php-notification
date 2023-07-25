03e1d8f0eb5086714af3ce6a7bff5622771dfeca
kriss <462679766@qq.com>
2023-07-25 18:58:56 +0800

    feature: add fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * This document has been generated with
+ * https://mlocati.github.io/php-cs-fixer-configurator/#version:3.13.1|configurator
+ * you can change this configuration by importing this file.
+ */
+$config = new PhpCsFixer\Config();
+$allowRisky = true;
+
+return $config
+    ->setLineEnding("\n")
+    ->setCacheFile(__DIR__.'/.php-cs-fixer.cache')
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->in([
+                __DIR__.'/src',
+            ])
+    )
+    ->setRules([
+        '@Symfony' => true,
+        '@Symfony:risky' => $allowRisky,
+        // '@PHP74Migration' => true,
+    ])
+    ->setRiskyAllowed($allowRisky);
